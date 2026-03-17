ff11acef22b4b6f9d61c66678028e55e71dfe905
nurhikmahulya <ulyapnup@gmail.com>
2026-03-17 23:03:35 +0800

    Add files via upload

---
+++ script.js
@@ -0,0 +1,317 @@
+const anggota = [
+  {
+    id: 1,
+    nama_panggilan: "Alda",
+    nama_lengkap: "Alda Nurdania",
+    tanggal_lahir: "20-02-2004",
+    foto: "images/alda.JPEG",
+    kelas: "IPA 3",
+    quote: "aldanrdna"
+  },
+  {
+    id: 2,
+    nama_panggilan: "Ama",
+    nama_lengkap: "DK Nurmuliana Safitry As",
+    tanggal_lahir: "18-09-2003",
+    foto: "images/ama.JPEG",
+    kelas: "IPS 2",
+    quote: "nurmuliaana"
+  },
+  {
+    id: 3,
+    nama_panggilan: "Amel",
+    nama_lengkap: "Ameliya",
+    tanggal_lahir: "27-09-2003",
+    foto: "images/amel.JPEG",
+    kelas: "IPS 2",
+    quote: "ameliyarfk"
+  },
+  {
+    id: 4,
+    nama_panggilan: "Andica",
+    nama_lengkap: "Andi Aurannisa Fatmawati",
+    tanggal_lahir: "24-08-2003",
+    foto: "images/andica.JPEG",
+    kelas: "IPA 3",
+    quote: "auranisa_fatmawati"
+  },
+  {
+    id: 5,
+    nama_panggilan: "Andis",
+    nama_lengkap: "Andi Khairunnisa",
+    tanggal_lahir: "28-03-2004",
+    foto: "images/andis.JPEG",
+    kelas: "IPS 2",
+    quote: "theycallmeandiss"
+  },
+  {
+    id: 6,
+    nama_panggilan: "Anjel",
+    nama_lengkap: "Anjelita",
+    tanggal_lahir: "09-10-2003",
+    foto: "images/anjel.JPEG",
+    kelas: "IPS 2",
+    quote: "anjelitaaa__09"
+  },
+  {
+    id: 7,
+    nama_panggilan: "Ate",
+    nama_lengkap: "Andi Altia Kusuma Dewi",
+    tanggal_lahir: "01-01-2005",
+    foto: "images/ate.JPEG",
+    kelas: "IPA 3",
+    quote: "andi.altia_"
+  },
+  {
+    id: 8,
+    nama_panggilan: "Aya",
+    nama_lengkap: "Sri Nurul Hidayah",
+    tanggal_lahir: "30-04-2004",
+    foto: "images/aya.JPEG",
+    kelas: "IPA 2",
+    quote: "_yayaaay4"
+  },
+  {
+    id: 9,
+    nama_panggilan: "Cica",
+    nama_lengkap: "Nurul Asisah",
+    tanggal_lahir: "01-03-2004",
+    foto: "images/cica.JPEG",
+    kelas: "IPS 2",
+    quote: "_aasisah"
+  },
+  {
+    id: 10,
+    nama_panggilan: "Eka",
+    nama_lengkap: "Eka Puspita Adnur",
+    tanggal_lahir: "14-03-2004",
+    foto: "images/eka.JPEG",
+    kelas: "IPA 2",
+    quote: "ekapsptadnr"
+  },
+  {
+    id: 11,
+    nama_panggilan: "Elip",
+    nama_lengkap: "Valina Jamaluddin",
+    tanggal_lahir: "31-05-2004",
+    foto: "images/elip.JPEG",
+    kelas: "IPA 2",
+    quote: "valina_jamaluddin"
+  },
+  {
+    id: 12,
+    nama_panggilan: "Hike",
+    nama_lengkap: "Nurhikma Hakim",
+    tanggal_lahir: "06-12-2004",
+    foto: "images/hike.JPEG",
+    kelas: "IPA 2",
+    quote: "nurhikmahakim_"
+  },
+  {
+    id: 13,
+    nama_panggilan: "Ilmi",
+    nama_lengkap: "Ilmi Sukni Ramadani",
+    tanggal_lahir: "26-10-2004",
+    foto: "images/ilmi.JPEG",
+    kelas: "IPA 2",
+    quote: "ilmisukni_"
+  },
+  {
+    id: 14,
+    nama_panggilan: "Ima",
+    nama_lengkap: "Maftuha Husnaeni",
+    tanggal_lahir: "30-12-2002",
+    foto: "images/ima.JPEG",
+    kelas: "IPS 2",
+    quote: "maftuhahsnn_"
+  },
+  {
+    id: 15,
+    nama_panggilan: "Kina",
+    nama_lengkap: "Andi Nur Ilmih Sakinah",
+    tanggal_lahir: "12-03-2004",
+    foto: "images/kina.JPEG",
+    kelas: "IPA 3",
+    quote: "_khyongg"
+  },
+  {
+    id: 16,
+    nama_panggilan: "Nisa",
+    nama_lengkap: "Anisa Filsa",
+    tanggal_lahir: "18-03-2004",
+    foto: "images/nisa.JPEG",
+    kelas: "IPS 2",
+    quote: "nisafils_sf"
+  },
+  {
+    id: 17,
+    nama_panggilan: "Noya",
+    nama_lengkap: "Nadya Fitrah",
+    tanggal_lahir: "05-03-2004",
+    foto: "images/noya.JPEG",
+    kelas: "IPA 3",
+    quote: "nadyaftrh_"
+  },
+  {
+    id: 18,
+    nama_panggilan: "Pute",
+    nama_lengkap: "Febriyanty Putri Zasilia",
+    tanggal_lahir: "27-02-2004",
+    foto: "images/pute.JPEG",
+    kelas: "IPS 2",
+    quote: "putrizaaasssh_"
+  },
+  {
+    id: 19,
+    nama_panggilan: "Rika",
+    nama_lengkap: "Rika Zhahirah",
+    tanggal_lahir: "04-10-2004",
+    foto: "images/rika.JPEG",
+    kelas: "IPA 3",
+    quote: "rkaaazhr___"
+  },
+  {
+    id: 20,
+    nama_panggilan: "Rina",
+    nama_lengkap: "Rina Asmi",
+    tanggal_lahir: "26-08-2003",
+    foto: "images/rina.JPEG",
+    kelas: "IPS 2",
+    quote: "rinaasmi_"
+  },
+   {
+    id: 21,
+    nama_panggilan: "Selep",
+    nama_lengkap: "Selvi Nafila",
+    tanggal_lahir: "13-10-2004",
+    foto: "images/selvi.JPEG",
+    kelas: "IPS 2",
+    quote: "selvynafilaa_"
+  },
+   {
+    id: 22,
+    nama_panggilan: "Tea",
+    nama_lengkap: "Siti Mutia Nurpadilah",
+    tanggal_lahir: "03-03-2005",
+    foto: "images/tea.JPEG",
+    kelas: "IPA 2",
+    quote: "mutiapadh"
+  },
+   {
+    id: 23,
+    nama_panggilan: "Tiara",
+    nama_lengkap: "Tiara Fadlyah Hamid",
+    tanggal_lahir: "06-10-2004",
+    foto: "images/tiara.JPEG",
+    kelas: "IPA 3",
+    quote: "tiarafadlyah"
+  },
+   {
+    id: 24,
+    nama_panggilan: "Ulya",
+    nama_lengkap: "Sitti Nurhikmah Ulya",
+    tanggal_lahir: "24-08-2004",
+    foto: "images/ulya.JPEG",
+    kelas: "IPS 2",
+    quote: "nurhikmah_ulya"
+  },
+   {
+    id: 25,
+    nama_panggilan: "Mumu",
+    nama_lengkap: "Ummu Kalsum",
+    tanggal_lahir: "30-11-2003",
+    foto: "images/ummu.JPEG",
+    kelas: "IPA 2",
+    quote: "_ummukalsumm"
+  },
+   {
+    id: 26,
+    nama_panggilan: "Yunna",
+    nama_lengkap: "Ainur Gunna",
+    tanggal_lahir: "26-05-2004",
+    foto: "images/yunna.JPEG",
+    kelas: "IPA 2",
+    quote: "ainur.yna"
+  }
+];
+// NAVIGASI
+function masuk() {
+  document.getElementById("welcome").style.display = "none";
+  document.getElementById("main").style.display = "block";
+}
+
+function kembali() {
+  document.getElementById("main").style.display = "none";
+  document.getElementById("welcome").style.display = "flex";
+}
+
+// FUNGSI
+function getHari(tgl) {
+  const hari = ["Minggu","Senin","Selasa","Rabu","Kamis","Jumat","Sabtu"];
+  const [d,m,y] = tgl.split("-");
+  return hari[new Date(`${y}-${m}-${d}`).getDay()];
+}
+
+function getTanggalLengkap(tgl) {
+  const bulan = ["Januari","Februari","Maret","April","Mei","Juni","Juli","Agustus","September","Oktober","November","Desember"];
+  const [d,m,y] = tgl.split("-");
+  return `${d} ${bulan[m-1]} ${y}`;
+}
+
+function getUmur(tgl) {
+  const [d,m,y] = tgl.split("-");
+  const b = new Date(`${y}-${m}-${d}`);
+  const t = new Date();
+  let u = t.getFullYear() - b.getFullYear();
+  if (t < new Date(t.getFullYear(), m-1, d)) u--;
+  return u;
+}
+
+function getUltahBulanIni() {
+  const bulan = new Date().getMonth() + 1;
+  let hasil = anggota.filter(a => parseInt(a.tanggal_lahir.split("-")[1]) === bulan);
+
+  hasil.sort((a,b) => parseInt(a.tanggal_lahir) - parseInt(b.tanggal_lahir));
+  return hasil;
+}
+
+// RENDER
+const container = document.getElementById("container");
+
+anggota.forEach(data => {
+  const card = document.createElement("div");
+  card.className = "card";
+
+  card.innerHTML = `
+    <img src="${data.foto}">
+    <h3>${data.nama_panggilan}</h3>
+  `;
+
+  card.onclick = () => {
+    document.getElementById("modal").style.display = "block";
+    document.getElementById("modalContent").innerHTML = `
+      <img src="${data.foto}" class="profile-img">
+      <h2>${data.nama_lengkap}</h2>
+      <p>${getHari(data.tanggal_lahir)}, ${getTanggalLengkap(data.tanggal_lahir)}</p>
+      <p>${getUmur(data.tanggal_lahir)} tahun</p>
+      <p><b>Alumni ${data.kelas}</b></p>
+      <p class="quote">@${data.quote}</p>
+    `;
+  };
+
+  container.appendChild(card);
+});
+
+// ULTAH
+const ultah = getUltahBulanIni();
+if (ultah.length > 0) {
+  document.getElementById("ultah").innerText =
+    "Birthday this month: " + ultah.map(a => a.nama_panggilan).join(", ");
+}
+
+// TUTUP MODAL
+window.onclick = (e) => {
+  if (e.target.id === "modal") {
+    document.getElementById("modal").style.display = "none";
+  }
+};
